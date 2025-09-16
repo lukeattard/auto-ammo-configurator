0b42509aa8ec02f2cc09cafd99a4570ec45541c8
Luke Attard <luke@rapiddata.com.au>
2025-09-16 20:00:15 +1000

    Update data-final-fixes.lua
    Added check if startup setting exists before setting the value, avoids errors at startup

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,13 +1,14 @@
--- data.raw.item["gun-turret"].automated_ammo_count = settings.startup["auto-ammo-loader-gun"].value
--- data.raw.item["rocket-turret"].automated_ammo_count = settings.startup["auto-ammo-loader-rocket"].value
-local value = 50
+local value = 10
 
 for name, ammo_turret in pairs(data.raw["ammo-turret"]) do
-	value = settings.startup["auto-ammo-loader-" .. name].value
-  if ammo_turret.automated_ammo_count >= value then
-    log(string.format("%s (%d)", ammo_turret.name, ammo_turret.automated_ammo_count))
-  else
-    log(string.format("%s (%d -> %d)", ammo_turret.name, ammo_turret.automated_ammo_count, value))
-    ammo_turret.automated_ammo_count = value
-  end
+	
+  if settings.startup["auto-ammo-loader-" .. name] then
+		value = settings.startup["auto-ammo-loader-" .. name].value
+		if ammo_turret.automated_ammo_count >= value then
+			log(string.format("%s (%d)", ammo_turret.name, ammo_turret.automated_ammo_count))
+		else
+			log(string.format("%s (%d -> %d)", ammo_turret.name, ammo_turret.automated_ammo_count, value))
+			ammo_turret.automated_ammo_count = value
+		end
+	end
 end
